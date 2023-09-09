7f9183c0e007d77d7aa001712a505a37e456bb0f
Atendiendo <huertamiguel527@gmail.com>
2023-09-09 17:07:06 -0300

    ahora si xde

--- Tarea2LP_202273602-k/Bomba.c
+++ Tarea2LP_202273602-k/Bomba.c
@@ -99,53 +99,6 @@ void ExplosionPunto(int fila, int columna){
 *   No se retorna nada ya que la función es de tipo void.
 *****/
 void ExplosionX(int fila, int columna){
-    // fila -=1;
-    // columna -=1;
-
-    // Bomba* bomba1 = (Bomba*)tablero[fila-1][columna-1];
-    // bomba1->contador_turnos -=1;
-    // if (bomba1->tierra_debajo->vida==0){
-    //     BorrarBomba(fila+1,columna+1);
-    // }
-    // if (bomba1->tierra_debajo->vida==0 && bomba1->tierra_debajo->es_tesoro==1){
-    //     cant_tesoros +=1;
-    // }
-
-    // Bomba* bomba2 = (Bomba*)tablero[fila+1][columna+1];
-    // bomba2->contador_turnos -=1;
-    // if (bomba2->tierra_debajo->vida==0){
-    //     BorrarBomba(fila+1,columna+1);
-    // }
-    // if (bomba2->tierra_debajo->vida==0 && bomba2->tierra_debajo->es_tesoro==1){
-    //     cant_tesoros +=1;
-    // }
-
-    // Bomba* bomba3 = (Bomba*)tablero[fila-1][columna+1];
-    // bomba3->contador_turnos -=1;
-    // if (bomba3->tierra_debajo->vida==0){
-    //     BorrarBomba(fila+1,columna+1);
-    // }
-    // if (bomba3->tierra_debajo->vida==0 && bomba3->tierra_debajo->es_tesoro==1){
-    //     cant_tesoros +=1;
-    // }
-
-    // Bomba* bomba4 = (Bomba*)tablero[fila+1][columna-1];
-    // bomba4->contador_turnos -=1;
-    // if (bomba4->tierra_debajo->vida==0){
-    //     BorrarBomba(fila+1,columna+1);
-    // }
-    // if (bomba4->tierra_debajo->vida==0 && bomba4->tierra_debajo->es_tesoro==1){
-    //     cant_tesoros +=1;
-    // }
-
-    // Bomba* bomba5 = (Bomba*)tablero[fila][columna];
-    // bomba5->contador_turnos -=1;
-    // if (bomba5->tierra_debajo->vida==0){
-    //     BorrarBomba(fila+1,columna+1);
-    // }
-    // if (bomba5->tierra_debajo->vida==0 && bomba5->tierra_debajo->es_tesoro==1){
-    //     cant_tesoros +=1;
-    // }
 
     return;
 }
